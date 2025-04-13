807fd0205f2ad356eb57891caca17f7f3bdad5e5
Zikam <isaac.aku@cyphercrescent.com>
2025-04-13 23:53:35 +0100

    ft: addded the form googlee to explore

--- components/home.vue
+++ components/home.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="bg-gray-100 text-gray-900">
-  <section class="relative h-[50vh] md:h-[70vh] bg-[url('/assets/bgg.jpg')] bg-cover bg-center text-white flex items-center justify-center">
-    <div class="absolute inset-0 bg-black bg-opacity-10"></div>
-    <div class="relative z-10 text-center px-4 sm:px-6 md:px-12 flex flex-col items-center justify-center">
-      <h1 class="text-4xl sm:text-5xl md:text-6xl font-extrabold leading-tight">
-        Elevate Your Style with  
-        <span class="text-white">Fashion</span>
-      </h1>
-      <p class="mt-4 text-base sm:text-lg md:text-xl max-w-2xl mx-auto">
-        Discover exclusive fashion collections crafted to redefine elegance.
-      </p>
-      <button
-        class="mt-6 px-6 py-2 sm:px-8 sm:py-3 bg-indigo-500 text-white text-base sm:text-lg font-semibold rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300">
-        Explore Now
-      </button>
-    </div>
-  </section>
-</div>
+    <section class="relative h-[50vh] md:h-[70vh] bg-[url('/assets/bgg.jpg')] bg-cover bg-center text-white flex items-center justify-center">
+      <div class="absolute inset-0 bg-black bg-opacity-10"></div>
+      <div class="relative z-10 text-center px-4 sm:px-6 md:px-12 flex flex-col items-center justify-center">
+        <h1 class="text-4xl sm:text-5xl md:text-6xl font-extrabold leading-tight">
+          Elevate Your Style with  
+          <span class="text-white">Fashion</span>
+        </h1>
+        <p class="mt-4 text-base sm:text-lg md:text-xl max-w-2xl mx-auto">
+          Discover exclusive fashion collections crafted to redefine elegance.
+        </p>
+        <a 
+          href="https://docs.google.com/forms/d/1MVkPgujjcCKfvgSramwpx9sOWIiOwhAFi6_2lBLxGlw/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="mt-6 px-6 py-2 sm:px-8 sm:py-3 bg-indigo-500 text-white text-base sm:text-lg font-semibold rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300"
+        >
+          Explore Now
+        </a>
+      </div>
+    </section>
+  </div>
 </template>
